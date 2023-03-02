1f4ff001cca29780abd2f363bf930503f695a500
DW <dnldp44@naver.com>
2023-03-02 17:12:28 +0900

    🔐 0302React (useState)

--- reactbasic/src/App.js
+++ reactbasic/src/App.js
@@ -1,11 +1,75 @@
-/*elint-disable*/
+/*eslint-disable*/
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  let user = 'Hoseong In'
+  // react에서 변수처럼 자료를 보관할 수 있는 state문법
+  let [count, setCount] = useState(0);
+  let [click, setClick] = useState(0);
+  // 변수에 들어있는 데이터 html에 넣기
+  // jsx 중괄호 문법사용
+  // 한 곳에다가 여러가지 자료를 저장하고 싶을 때 Array
+  let [title, settitle] = useState(["DW아카데미 503호", 'DW아카데미 501호', 'DW아카데미 502호']);
+  // const style = {
+  //   backgroundColor : 'blue', 
+  //   fonSize : '24px',
+  //   fontWeight : 'bold',
+  //   // width : '100px',
+  //   // height: '100px'
+  // }; 
+
   return (
     <div className="App">
-      <h1></h1>
+       {/* <div className='box' style={style}>zdzd</div>
+       <input type="text" />
+       <div style={{color:'red', backgroundColor: 'gray', width:'100px', height:'200px'}}>안녕하세요?</div> */}
+       <h1>Hello, {user}!</h1>
+       <p>This is a React App</p>
+
+       <div>
+        <p>클릭시 + {count}</p>
+
+        <button onClick={()=>{
+          setCount(count + 1)
+        }}>Click Me</button>
+
+       </div>
+
+       <div className='title'>
+        <h4>{title[0]}</h4>
+        <span onClick={() => {setClick(click + 1)}}>👍{click}</span>
+        <p>안녕하세요 저는 인호성입니다.</p>
+       </div>
+
+       <div className='title'>
+        <h4>{title[1]}</h4>
+        <span onClick={() => {setClick(click + 1)}}>👍{click}</span>
+        <p>안녕하세요 저는 인호성입니다.</p>
+       </div>
+
+       <div className='title'>
+
+        <h4>{title[2]}</h4>
+
+        <span onClick={() => {setClick(click + 1)}}>👍{click}</span>
+        <p>안녕하세요 저는 인호성입니다.</p>
+       </div>
+
+       <button onClick={() => {
+        let copy = [...title]
+        copy[0] = '리액트 너무 재밌어요!';
+        copy[1] = '리액트 너무 어려운걸요~';
+        settitle(copy)
+       }}>글 제목 변경</button>
+       
+       <button onClick={()=> {
+        let copy = [...title]
+        copy = copy.sort()
+        settitle(copy)
+       }}>글정렬</button>
+
     </div>
   );
 }
