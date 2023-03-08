4a50c54044bc2163b121fdb279447bad9d7aee89
DW <dnldp44@naver.com>
2023-03-08 17:15:32 +0900

    ✅ 0308React

--- reactbasic/src/component/CommentList.jsx
+++ reactbasic/src/component/CommentList.jsx
@@ -20,6 +20,10 @@ const comments = [
 function CommentList() {
   const [commentList, setCommentList] = useState(comments);
 
+  // input 선생님 // 
+  const [name , setName] = useState('');
+  const [content , setContent] = useState('');
+
   const deleteComment = (index) => {
     const newCommnetList =[...commentList];
     newCommnetList.splice(index, 1)
@@ -28,26 +32,32 @@ function CommentList() {
 
   /////// 추가 버튼 클릭했을 때 박스 /////
   const addComment = () => {
-    const add = {name : '여상현', comment : '리액트 어렵다~~!' }
+    // const add = {name : '여상현', comment : '리액트 어렵다~~!' }
+    // setCommentList([add, ...commentList])
+    const add = {name : name, comment : content}
     setCommentList([add, ...commentList])
+    // state변경 함수에 빈 문자열을 넣어서 input에 작성된 내용을 초기화
+    setName('');
+    setContent('');
   }
   
+  // input에 입력했을 때 나오게 하는것 ////
+  // const [idValue, setId] = useState();
+  // const [pwValue, setPw] = useState();
 
-  ///// input에 입력했을 때 나오게 하는것 ////
-  const [idValue, setId] = useState();
-  const [pwValue, setPw] = useState();
-
-  const saveId = (e) => {
-    setId(e.target.value)
-  }
-  const savePw = (e) => {
-    setPw(e.target.value)
-  }
+  // const saveId = (e) => {
+  //   setId(e.target.value)
+  // }
+  // const savePw = (e) => {
+  //   setPw(e.target.value)
+  // }
   
-  const addtyping = () => {
-    const typing = [...commentList, {name : idValue, comment : pwValue}]
-    setCommentList(typing)
-  }
+  // const addtyping = () => {
+  //   const typing = [...commentList, {name : idValue, comment : pwValue}]
+  //   setCommentList(typing)
+  //   setId('')
+  //   setPw('')
+  // }
 
 
   return (
@@ -59,10 +69,16 @@ function CommentList() {
           )
         })
       }
+      {/* onChange 이벤트는 input 요소에서 값이 변경될 때 발생 
+          그 때 발생한 이벤트를 매개변수 e */}
+      이름 : <input type="text" value={name} onChange={(e)=> setName(e.target.value)} />
+      댓글 : <input type="text" value={content} onChange={(e)=> setContent(e.target.value)} />
       <button onClick={addComment}>추가</button>
-      <p>이름: <input onChange={saveId} type="text" value={idValue} /></p>
+
+      {/* <p>이름: <input onChange={saveId} type="text" value={idValue} /></p>
       <p>댓글: <input onChange={savePw} type="text" value={pwValue} /></p>
-      <button onClick={addtyping}>글쓰기</button>
+      <button onClick={addtyping}>글쓰기</button> */}
+
     </div>
   )
 }
